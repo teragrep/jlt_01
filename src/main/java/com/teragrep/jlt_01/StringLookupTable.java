440a8f9a7eb29db00d0352fc4e3ceb3579499c20
Motoko Kusanagi <kordex@gmail.com>
2023-01-26 17:23:54 +0200

    more testing, nomatch handled now

--- src/main/java/com/teragrep/jlt_01/StringLookupTable.java
+++ src/main/java/com/teragrep/jlt_01/StringLookupTable.java
@@ -9,16 +9,33 @@ import java.util.HashMap;
 public class StringLookupTable {
 
     private final HashMap<String, String> stringLookup = new HashMap<>();
+    private final String noMatch;
     public StringLookupTable(BufferedReader bufferedReader) {
         Gson gson = new Gson();
 
         JsonStringLookupTable lookupTable = gson.fromJson(bufferedReader, JsonStringLookupTable.class);
 
-        for (int i = 0; i < lookupTable.getTable().size(); i++) {
+        if ( lookupTable.getNomatch() == null) {
+            throw new  IllegalArgumentException(".nomatch missing");
+        }
+        else {
+            noMatch = lookupTable.getNomatch();
+        }
+
+        if ( lookupTable.getTable() == null) {
+            throw new IllegalArgumentException(".table missing");
+        }
+        else {
+            for (int i = 0; i < lookupTable.getTable().size(); i++) {
                 stringLookup.put(lookupTable.getTable().get(i).getIndex(), lookupTable.getTable().get(i).getValue());
+            }
         }
     }
     public String lookup(String expression) {
-        return stringLookup.get(expression);
+        String result = stringLookup.get(expression);
+        if (result == null) {
+            result = noMatch;
+        }
+        return result;
     }
 }
--- src/test/java/com/teragrep/jlt_01/StringLookupTest.java
+++ src/test/java/com/teragrep/jlt_01/StringLookupTest.java
@@ -1,5 +1,6 @@
 package com.teragrep.jlt_01;
 
+import com.google.gson.JsonSyntaxException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +11,7 @@ import java.io.FileReader;
 public class StringLookupTest {
 
     @Test
-    public void testLookupLoading() throws FileNotFoundException {
+    public void stringTable() throws FileNotFoundException {
         BufferedReader bufferedReader = new BufferedReader(
                 new FileReader("src/test/resources/good/string.json"));
 
@@ -18,6 +19,75 @@ public class StringLookupTest {
         Assertions.assertEquals("un", stringLookupTable.lookup("one"));
         Assertions.assertEquals("deux", stringLookupTable.lookup("two"));
         Assertions.assertEquals("trois", stringLookupTable.lookup("three"));
+    }
+    @Test
+    public void stringTableNoMatch() throws FileNotFoundException {
+        BufferedReader bufferedReader = new BufferedReader(
+                new FileReader("src/test/resources/good/string.json"));
+
+        StringLookupTable stringLookupTable = new StringLookupTable(bufferedReader);
+
+        Assertions.assertEquals("unknown", stringLookupTable.lookup("not-there-at-all"));
+    }
+
+    @Test
+    public void tableNull() throws FileNotFoundException {
+        BufferedReader bufferedReader = new BufferedReader(
+                new FileReader("src/test/resources/bad/table_null.json"));
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            StringLookupTable stringLookupTable = new StringLookupTable(bufferedReader);
+        });
+    }
+
+    @Test
+    public void notJson() throws FileNotFoundException {
+        BufferedReader bufferedReader = new BufferedReader(
+                new FileReader("src/test/resources/bad/not_json.json"));
+
+        Assertions.assertThrows(JsonSyntaxException.class, () -> {
+            StringLookupTable stringLookupTable = new StringLookupTable(bufferedReader);
+        });
+    }
+
+    @Test
+    public void nomatchMissing() throws FileNotFoundException {
+        BufferedReader bufferedReader = new BufferedReader(
+                new FileReader("src/test/resources/bad/nomatch_missing.json"));
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            StringLookupTable stringLookupTable = new StringLookupTable(bufferedReader);
+        });
+    }
+
+    @Test
+    public void valueArray() throws FileNotFoundException {
+        BufferedReader bufferedReader = new BufferedReader(
+                new FileReader("src/test/resources/bad/value_array.json"));
+
+        Assertions.assertThrows(JsonSyntaxException.class, () -> {
+            StringLookupTable stringLookupTable = new StringLookupTable(bufferedReader);
+        });
+    }
+
+    @Test
+    public void valueObject() throws FileNotFoundException {
+        BufferedReader bufferedReader = new BufferedReader(
+                new FileReader("src/test/resources/bad/value_object.json"));
+
+        Assertions.assertThrows(JsonSyntaxException.class, () -> {
+            StringLookupTable stringLookupTable = new StringLookupTable(bufferedReader);
+        });
+    }
+
+    @Test
+    public void stringTableEmpty() throws FileNotFoundException {
+        BufferedReader bufferedReader = new BufferedReader(
+                new FileReader("src/test/resources/good/string_empty.json"));
+
+        StringLookupTable stringLookupTable = new StringLookupTable(bufferedReader);
+
+        Assertions.assertEquals("unknown", stringLookupTable.lookup("not-a-thing"));
     }
 }
 
